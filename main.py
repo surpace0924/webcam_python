b05ac11c73c36b0449b8980938c8fb868e176671
surpace0924 <surpace2479@gmail.com>
2024-02-19 16:45:34 +0900

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 def save_webcam_image():
-    
+    # 保存間隔を設定
     dt = 10
 
     # ウェブカメラにアクセス
@@ -17,16 +17,17 @@ def save_webcam_image():
         # 現在の時間を取得
         current_time = time.time()
 
-        # 1分経過したかを確認し、経過していれば画像を保存
+        # 時間経過したかを確認し、経過していれば画像を保存
         if current_time - last_save_time >= dt:
-            # 保存用のファイル名を作成
-            current_time_str = time.strftime("%Y%m%d%H%M%S")
-            filename = os.path.join('captured', f'{current_time_str}.jpg')
 
             # カメラから画像を取得
             ret, frame = cap.read()
 
             if ret:
+                # 保存用のファイル名を作成
+                current_time_str = time.strftime("%Y%m%d%H%M%S")
+                filename = os.path.join('captured', f'{current_time_str}.jpg')
+                
                 # 画像を保存
                 cv2.imwrite(filename, frame)
                 print(f"Saved: {filename}")
